9c34f41f9554371f0639b3ea89a974a6178e14ae
Hanna Lee <mani21na@gmail.com>
2019-11-04 20:38:01 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,51 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :index
   end
+  
+  #CREATE
+  get '/articles/new' do
+    erb :new
+  end
+  
+  post '/articles' do
+    @article = Aticles.new(params)
+    erb :show 
+  end
+  
+  #READ
+  get '/articles' do
+    @articles = Ariticle.all
+    erb :index
+  end 
+  
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end 
+  
+  #UPDATE
+  get '/articles/:id/edit' do
+    @article = Article.find(params["id"])
+    erb :edit
+  end
+  
+  patch '/articles/:id' do
+    id = params["id"]
+    new_params = {}
+    old_article = Article.find(id)
+    new_params[:name] = params["name"]
+    new_params[:content] = params["content"]
+    old_article.update(new_params)
+
+    redirect "/posts/#{id}"
+  end
+  
+  #DELDTE
+  delete '/articles/:id' do
+    @article = Article.find(params["id"])
+    @article.destroy
+    erb :index
+  end 
 end
